d6ac9fcab654c2f99fd5d9dd86ca6b331a542a01
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-04-01 16:46:53 -0500

    changes

--- src/main/java/com/runtimeverification/mvnplugin/ErrorCodesMojo.java
+++ src/main/java/com/runtimeverification/mvnplugin/ErrorCodesMojo.java
@@ -1,26 +1,39 @@
 package com.runtimeverification.mvnplugin;
+import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
-
-/**
- * The array contatining the location of the K Files.
- */
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Pattern;
 
 
 @Mojo( name = "sayhi")
 public class ErrorCodesMojo extends AbstractMojo
 {
 
-    @Parameter
-    private File[] directories;
+    /**
+     * The array contatining the location of the K Files.
+     */
+    @Parameter(defaultValue = "${project.basedir}" + "../c-semantics")
+    private File semanticsDir;
 
 
     public void execute() throws MojoExecutionException
     {
-        System.out.println(directories[0].toString());
+        getCodesFromKFiles(semanticsDir);
+    }
+
+    private Collection<File> getCodesFromKFiles(File baseDir) {
+        Set<String> codesSet = new HashSet<>();
+        Collection<File> kFiles = FileUtils.listFiles(baseDir, new String[]{"k"}, true);
+        Pattern pattern = Pattern.compile("[A-Z]+[0-9]*");
+        kFiles.forEach(f -> {
+
+        });
     }
 }
