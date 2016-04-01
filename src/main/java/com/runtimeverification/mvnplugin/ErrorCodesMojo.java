edd004f0056a455a24399870749f012d53a0ac96
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-04-01 13:57:57 -0500

    added support for directories

--- src/main/java/com/runtimeverification/mvnplugin/ErrorCodesMojo.java
+++ src/main/java/com/runtimeverification/mvnplugin/ErrorCodesMojo.java
@@ -2,16 +2,25 @@ package com.runtimeverification.mvnplugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import java.io.File;
 
 /**
- * Says "Hi" to the user.
- *
+ * The array contatining the location of the K Files.
  */
+
+
 @Mojo( name = "sayhi")
 public class ErrorCodesMojo extends AbstractMojo
 {
+
+    @Parameter
+    private File[] directories;
+
+
     public void execute() throws MojoExecutionException
     {
-        getLog().info( "Hello, world." );
+        System.out.println(directories[0].toString());
     }
 }
