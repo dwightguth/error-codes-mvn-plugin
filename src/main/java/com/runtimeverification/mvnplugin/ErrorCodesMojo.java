a2d289ec356070581d9af6c41988b24fe0c3df14
Manasvi Saxena <manasvi.saxena@runtimeverification.com>
2016-04-04 14:15:12 -0500

    refined the Regexes in the MOJO

--- src/main/java/com/runtimeverification/mvnplugin/ErrorCodesMojo.java
+++ src/main/java/com/runtimeverification/mvnplugin/ErrorCodesMojo.java
@@ -1,9 +1,6 @@
 package com.runtimeverification.mvnplugin;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.filefilter.FileFilterUtils;
-import org.apache.commons.io.filefilter.IOFileFilter;
-import org.apache.commons.io.filefilter.NameFileFilter;
-import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.apache.commons.io.filefilter.*;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -12,10 +9,12 @@ import org.apache.maven.plugins.annotations.Parameter;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 
 @Mojo( name = "sayhi")
@@ -31,7 +30,7 @@ public class ErrorCodesMojo extends AbstractMojo
 
     public void execute() throws MojoExecutionException
     {
-        System.out.println(getCodesFromCSV(semanticsDir).size());
+        System.out.println(getCodesFromFiles(semanticsDir).size());
 
     }
 
@@ -41,8 +40,18 @@ public class ErrorCodesMojo extends AbstractMojo
     }
 
     private Set<String> getCodesFromCSV(File baseDir) {
-        Collection<File> csvFiles = FileUtils.listFiles(baseDir, FileFilterUtils.nameFileFilter("Error_Codes.csv"), TrueFileFilter.INSTANCE);
+        Collection<File> csvFiles = FileUtils.listFiles(baseDir,
+                FileFilterUtils.nameFileFilter("Error_Codes.csv"), TrueFileFilter.INSTANCE);
         return getErrorCodesFromFiles(csvFiles);
+    }
+
+    private Set<String> getCodesFromFiles(File baseDir) {
+        Collection<File> exampleFiles = FileUtils.listFiles(baseDir,
+                new RegexFileFilter("(UB|CV|USP|IMPL|L)\\-([A-Z]{2,}[0-9]+)\\-bad.*\\.c"), TrueFileFilter.INSTANCE);
+        return exampleFiles.stream().map(x -> {
+            String s = x.toString();
+            return s.substring(s.lastIndexOf("/") + 1).split("\\-bad.*\\.c")[0];
+        }).collect(Collectors.toSet());
     }
 
     private Set<String> getErrorCodesFromFiles(Collection<File> files) {
